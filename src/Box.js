db308dd7e8c5471f5bd015ea813d13d668dae92a
Sam <super8989@gmail.com>
2019-10-05 21:27:57 -0400

    created a box component to render a single box with 1 colour

---
+++ src/Box.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./Box.css";
+
+class Box extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { color: "purple" };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {}
+
+  render() {
+    return (
+      <div
+        className="Box"
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      ></div>
+    );
+  }
+}
+
+export default Box;
