fb8201ffe20bf27b056a71e39a5925ae12d9efaf
Sam <super8989@gmail.com>
2019-10-05 21:59:51 -0400

    added styling to BoxContainer

--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
-import Box from './Box';
+import React, { Component } from "react";
+import Box from "./Box";
+import "./BoxContainer.css";
 
 class BoxContainer extends Component {
-    static defaultProps = {
-        numBoxes: 18
-    };
+  static defaultProps = {
+    numBoxes: 18,
+    allColors: ['PaleGreen', 'LightGreen', 'MediumSpringGreen', 'SpringGreen', 'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'YellowGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen'  ]
+  };
 
-    render() {
-        const boxes = Array.from({length: this.props.numBoxes}).map(() => (<Box />));
+  render() {
+    const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
+      <Box colors={this.props.allColors} />
+    ));
 
-        return <div className="BoxContainer">{boxes}</div>
-    }
+    return <div className="BoxContainer">{boxes}</div>;
+  }
 }
 
 export default BoxContainer;
