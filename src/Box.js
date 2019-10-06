b8e33a5b8e1b619a8ca867bfd93ea78e61d8a986
Sam <super8989@gmail.com>
2019-10-05 21:33:42 -0400

    random colours for the box

--- src/Box.js
+++ src/Box.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { choice } from './helpers';
 import "./Box.css";
 
 class Box extends Component {
+    static defaultProps = {
+        allColors: [ 'purple', 'magenta', 'violet', 'pink']
+    }
+
   constructor(props) {
     super(props);
-    this.state = { color: "purple" };
+    this.state = { color: choice(this.props.allColors) };
     this.handleClick = this.handleClick.bind(this);
   }
 
